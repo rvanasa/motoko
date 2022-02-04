7d1a9e47979eb6030b1c9e7ac83c8f163c547d88
Gabor Greif <gabor@dfinity.org>
2022-02-04 15:12:00 +0100

    normalise (heap|table) to "heap or table"
    since I get different responses from `drun` on macOS vs. Linux

--- test/run.sh
+++ test/run.sh
@@ -82,6 +82,7 @@ function normalize () {
     sed 's/^\(         [0-9]\+:\).*!/\1 /g' | # wasmtime backtrace locations
     sed 's/Ignore Diff:.*/Ignore Diff: (ignored)/ig' |
     sed 's/compiler (source .*)/compiler (source XXX)/ig' |
+    sed -E 's/trapped: (heap|table) out of bounds/trapped: heap or table out of bounds/ig' |
     # Normalize canister id prefixes in debug prints
     sed 's/\[Canister [0-9a-z\-]*\]/debug.print:/g' |
     # Normalize instruction locations on traps, added by ic-ref ad6ea9e
